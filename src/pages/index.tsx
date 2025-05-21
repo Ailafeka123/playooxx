28b434be0596615e368bea5d1a62f3eaa370b97f
Ailafeka <ailafeka@gmail.com>
2025-05-21 15:56:36 +0800

    test:測試Next使用page製作 並用Github Active執行
    修正src/app 更改名稱備份
    增加src/pages/index進行測試觀察

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head"
+
+export default function Home(){
+    return(
+    <>
+        <Head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <title>小遊戲網站</title>
+            <meta name="description" content="小遊戲區" />
+            <meta name="keywords" content="遊戲" />
+            <meta name="author" content="劉星緯" />
+        </Head>
+        <main>
+            <h1>測試用</h1>
+        </main>
+    </>)
+}
