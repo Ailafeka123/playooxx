4870a8fcc7c7c48aa909c274a64a4a53a6d18506
Ailafeka <ailafeka@gmail.com>
2025-05-21 16:00:14 +0800

    test: 修正index.tsx的charSet 重新測試

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home(){
     return(
     <>
         <Head>
-            <meta charset="UTF-8" />
+            <meta charSet="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta http-equiv="X-UA-Compatible" content="IE=edge" />
             <title>小遊戲網站</title>
