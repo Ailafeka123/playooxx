68f17ad3bd6ad1ebe1fee0540afdebe4d87f2b13
Ailafeka <ailafeka@gmail.com>
2025-05-21 18:26:43 +0800

    fix:修正config.ts 嘗試修正路徑

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: '/playooxx',
+  assetPrefix: '/playooxx',
+
 };
 
 export default nextConfig;
