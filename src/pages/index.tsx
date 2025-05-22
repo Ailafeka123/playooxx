31bfba600e658665d4649165b7c539ad57c225ac
Ailafeka <ailafeka@gmail.com>
2025-05-22 16:06:27 +0800

    fix:修正build錯誤
    移除:index.tsx移除沒有使用的SEOHead

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
-import SEOHead from "@/component/Head/Head"
 export default function Home(){
     return(
     <>  
-        {/* <SEOHead></SEOHead> */}
         <main>
             <h1>測試用</h1>
         </main>
